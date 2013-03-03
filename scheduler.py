793a588f230ad0302e200c5bf2bda0b84c707a80
Kenny Levinsen <kennylevinsen@gmail.com>
2013-03-03 20:30:42 +0100

    Slight refactor

--- scheduler.py
+++ scheduler.py
@@ -13,8 +13,8 @@ class eventScheduler(threading.Thread):
 	def run(self):
 		self.internalList = []
 		self.reloadEvents()
-		
-		while 1:
+
+		while True:
 			self.handleEvents()
 			next = self.nextEvent()
 			if next < 60:
@@ -26,15 +26,13 @@ class eventScheduler(threading.Thread):
 		with open(eventFile) as f:
 			self.events = json.loads(f.read())
 		self.internalList = []
+
 		for el in self.events:
-			self.updateEvent(el)
+			self.internalList.append(self.createEvent(el))
 
-
-	def updateEvent(self, event):
-		self.internalList.append(	{
-										"event": event,
-										"datetime": rrule(DAILY, count=1, byhour=event['time']['hours'], byminute=event['time']['minutes'], bysecond=0)[0]
-									})
+	def createEvent(self, event):
+		return { "event": event,
+					"datetime": rrule(DAILY, count=1, byhour=event['time']['hours'], byminute=event['time']['minutes'], bysecond=0)[0]}
 
 	def nextEvent(self):
 		secs = 0
@@ -42,14 +40,14 @@ class eventScheduler(threading.Thread):
 			temp = (event['datetime'] - datetime.datetime.now()).seconds
 			if temp > secs:
 				secs = temp
-		return secs		
+		return secs
 
 	def handleEvents(self):
-		for idx, event in enumerate(self.internalList):
+		tmp = self.internalList
+		for idx, event in enumerate(tmp):
 			if event['datetime'] < datetime.datetime.now() + datetime.timedelta(seconds=1):
-				print "Executing event " + event['event']['name']
-				self.internalList.pop(idx)
-				self.updateEvent(event['event'])
+				print('Executing event', event['event']['name'])
+				self.internalList[idx] = self.createEvent(event['event'])
 
 scheduler = eventScheduler()
 scheduler.start()
