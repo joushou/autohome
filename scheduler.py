54f5cbfed3028240eeb8cf051db6a78570a101e6
root <filipsandborg@gmail.com>
2013-03-04 08:08:26 +0100

    Readded silly hue_ip

--- scheduler.py
+++ scheduler.py
@@ -30,10 +30,12 @@ automators = {
 	'EXT': AutoSartano(127, switcher)
 }
 
-if hue_ip and hue_key:
+try:
 	automators.update({
 		'ROOM': AutoHue(hue_ip, hue_key, 2)
 	})
+except NameError:
+	pass
 
 def allOff():
 	for i in automators:
