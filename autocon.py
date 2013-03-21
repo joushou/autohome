215586c3ca50928fa9f880847956e3bc7760b4f9
Kenny Levinsen <kennylevinsen@gmail.com>
2013-03-21 22:16:57 +0100

    Encapsulate

--- autocon.py
+++ autocon.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python2.7
 from __future__ import print_function, absolute_import
 from datetime import datetime
-from json import loads
+from json import loads, dumps
 from serial import Serial
 from SocketServer import BaseRequestHandler, TCPServer
 from socket import timeout
 from sys import argv
-from automated import Automated, AutoSartano, AutoHue
+from automated import Automated, AutoSartano, AutoHue, AutoLG
 from scheduler import eventScheduler, event
 
 serfile = argv[1]
@@ -14,80 +14,117 @@ hwfile = argv[2]
 eventFile = argv[3]
 listenPort = int(argv[4])
 
-ser = Serial(serfile, 9600, timeout=1)
-def switcher(id, state):
-   ser.write(chr(state<<7|id))
+class AutoHome(object):
+	def __init__(s, serfile, hwfile, eventfile):
+		s.serfile = serfile
+		s.hwfile = hwfile
+		s.eventfile = eventfile
+		s.automators = {}
+		s.events = []
+		s.scheduler = eventScheduler()
+		s.prepare()
+		s.loadEvents()
 
-automators = {}
+	def list(self):
+		return self.automators
 
-with open(hwfile) as f:
-	x = loads(f.read())
-for i in x:
-	if i['type'] == 'AutoSartano':
-		automators[i['name']] = AutoSartano(i['params']['id'], switcher)
-	if i['type'] == 'AutoHue':
-		automators[i['name']] = AutoHue(**i['params'])
+	def on(self, key):
+		if key == 'ALL':
+			for i in self.automators:
+				i.on()
+				i.state = 'on'
+		else:
+			self.automators[key].on()
 
-events = []
-scheduler = eventScheduler()
+	def off(self, key):
+		if key == 'ALL':
+			for i in self.automators:
+				i.off()
+				i.state = 'off'
+		else:
+			self.automators[key].off()
 
-def handleEvent(_id):
-	global events
-	for i in events:
-		if i['id'] == _id:
-			for action in i['actions']:
-				print('[AUTOMATOR] Setting state of %s to %s' % (action['id'], action['state']))
-				automators[action['id']].set_state(action['state'])
-			break
-scheduler.listen(handleEvent)
+	def dim(self, key, dim):
+		self.automators[key].dim(dim)
+		self.automators[key].state = dim
 
-def loadEvents():
-	global events
-	with open(eventFile) as f:
-		events = loads(f.read())
-	for i in events:
-		timing = datetime.now().replace(hour=i['time']['hours'], minute=i['time']['minutes'], second=0, microsecond=0)
-		i['id'] = scheduler.getNewID()
-		scheduler.createEvent(event(timing, t='daily', _id=i['id']))
+	def prepare(self):
+		ser = Serial(serfile, 9600, timeout=1)
+		def switcher(_id, state):
+			ser.write(chr(state<<7|_id))
+
+		with open(hwfile) as f:
+			x = loads(f.read())
+		for i in x:
+			if i['type'] == 'AutoSartano':
+				self.automators[i['name']] = AutoSartano(i['params']['id'], switcher)
+			if i['type'] == 'AutoHue':
+				self.automators[i['name']] = AutoHue(**i['params'])
+			if i['type'] == 'AutoLG':
+				self.automators[i['name']] = AutoLG('/dev/ttyS0')
+			self.automators[i['name']].name = i['name']
+			self.automators[i['name']].type = i['type']
+
+		def handleEvent(_id):
+			for i in self.events:
+				if i['id'] == _id:
+					for action in i['actions']:
+						print('[AUTOMATOR] Setting state of %s to %s' % (action['id'], action['state']))
+						self.automators[action['id']].set_state(action['state'])
+					break
+		self.scheduler.listen(handleEvent)
+
+	def loadEvents(self):
+		with open(self.eventfile) as f:
+			self.events = loads(f.read())
+		for i in self.events:
+			timing = datetime.now().replace(hour=i['time']['hours'], minute=i['time']['minutes'], second=0, microsecond=0)
+			i['id'] = self.scheduler.getNewID()
+			self.scheduler.createEvent(event(timing, t='daily', _id=i['id']))
+
+auto = AutoHome(serfile, hwfile, eventFile)
 
 class TCPHandler(BaseRequestHandler):
 	def parseData(self, data):
 		part = data.partition('_')
-		if part[0] == 'ALL':
-			for i in automators:
-				if part[2] == 'ON':
-					automators[i].on()
-				else:
-					automators[i].off()
-		elif part[0] in automators:
-			if part[2] == 'ON':
-				automators[part[0]].on()
-			elif part[2] == 'OFF':
-				automators[part[0]].off()
-			else:
-				try:
-					automators[part[0]].dim(int(part[2]))
-				except:
-					pass
-			return 'OK\n'
+		if part[2] == 'ON':
+			auto.on(part[0])
+		elif part[2] == 'OFF':
+			auto.off(part[0])
 		else:
-			return 'ERROR\n'
+			auto.dim(part[0], int(part[2]))
+
+	def parseJSON(self, data):
+		a = loads(data)
+		if 'op' in a:
+			if a['op'] == 'list':
+				x = auto.list()
+				y = {}
+				for i in x:
+					y[x[i].name] = {'type': x[i].type, 'state': x[i].state}
+				return dumps(y)
+			elif a['op'] == 'on':
+				auto.on(a['name'])
+				return dumps({'status': 'ok'})
+			elif a['op'] == 'off':
+				auto.off(a['name'])
+				return dumps({'status': 'ok'})
+		return dumps({'status': 'error'})
 
 	def handle(self):
 		self.request.settimeout(1)
 		print("[AUTOMATOR] Connection from " + self.client_address[0])
 		try:
-			self.data = self.request.recv(1024).strip()
+			self.data = self.request.recv(10240)
 			print ("[AUTOMATOR] {} wrote:".format(self.client_address[0]))
 			print (self.data)
-			self.request.sendall(self.parseData(self.data))
+			self.request.sendall(self.parseJSON(self.data))
 		except timeout:
 			self.request.sendall("TIMEOUT\n")
 
 	def finish(self):
 		print("[AUTOMATOR] Connection closed")
 
-loadEvents()
 
 TCPServer.allow_reuse_address = True
 server = TCPServer(('0.0.0.0', listenPort), TCPHandler)
